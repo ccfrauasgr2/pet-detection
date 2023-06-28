3b90447428d776f891fb048d2c405b1c1a6b68f0
Steele <kien1512@gmail.com>
2023-06-28 20:44:59 +0200

    refactored code

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,38 +1,37 @@
 import os
-import requests
+import telegram
 import base64
+import asyncio
 
 
-def send_telegram_notification(detection_results):
+async def send_telegram_notification(detection_results):
     """
-    notifies user about detection results via Telegram Bot
+    notifies user about detection results via TNB
+
+    Notification = Image + Caption
     """
 
-    # Retrieve the bot token and chat ID from environment variables
+    # Retrieve the bot token and group chat ID from environment variables
     bot_token = base64.b64decode(os.environ['TELEGRAM_BOT_TOKEN']).decode("utf-8")
-    group_chat_id = base64.b64decode(os.environ['TELEGRAM_CHAT_ID']).decode("utf-8")
+    group_chat_id = "-" + base64.b64decode(os.environ['TELEGRAM_CHAT_ID']).decode("utf-8")
 
+    # Get image for notification
+    img = base64.b64decode(detection_results["picture"])
+
+    # Create caption for notification
     caption = "Detected following pet(s):"
     for det in detection_results["detections"]:
-
         bid = det["BID"]
         pet_type =  det["type"]
         accuracy = det["accuracy"]
-    
         temp = f"\nBID: {bid} - Type: {pet_type} - Accuracy: {accuracy}"
         caption += temp
 
-    img = base64.b64decode(detection_results["picture"])
-    print(img)
-    return
+    # Initialize the TNB
+    bot = telegram.Bot(token=bot_token)
 
-    url = f'https://api.telegram.org/bot{bot_token}/sendPhoto?chat_id={group_chat_id}&caption={caption}'
-    response = requests.post(url, files={'photo': img})
-    
-    if response.status_code == 200:
-        print('Notification sent successfully!')
-    else:
-        print('Failed to send notification.')
+    # Send detection results to the TNB
+    await bot.send_photo(chat_id=group_chat_id, photo=img, caption=caption)
 
 
 
@@ -61,4 +60,4 @@ detection_results = {
 }
 
 # Example usage
-send_telegram_notification(detection_results)
+asyncio.run(send_telegram_notification(detection_results))
